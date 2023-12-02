5154b16dd9f7403d773993930a01f701b9a537ad
luanngo9352 <luanngo9352@gmail.com>
2023-12-03 02:39:38 +0700

    {Register - Auth - Logout - Update,...} Users. Admin authentication. Get User profile

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
     image:{
         type: String,
         required: true,
+        default: false,
     },
     isAdmin:{
         type: Boolean,
@@ -31,6 +32,15 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
+
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
