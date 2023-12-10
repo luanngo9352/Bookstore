0188a54484d423f0933c00639383e598c1f37488
luanngo9352 <luanngo9352@gmail.com>
2023-12-10 13:46:58 +0700

    A bunch of functions added - Book Reviews & Ratting added.

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,10 +1,27 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema({
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User",
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
     },
     bookName:{
         type: String,
@@ -52,10 +69,23 @@ const productSchema = new mongoose.Schema({
         required: true,
         default: 0,
     },
-},{
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model('Product', productSchema);
 
 export default Product;
