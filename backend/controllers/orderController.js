67e02161510681970793f1fbfc42d38723cb83ee
luanngo9352 <luanngo9352@gmail.com>
2023-12-18 22:36:22 +0700

    Order Deliver API added.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -84,12 +84,24 @@ const updateOrderToPaid = asyncHandler (async (req, res)=> {
     }
 });
 
-// @desc     Update Order to delivered
-// @route    GET /api/orders/:id/pay
-// @access   Private
-const updateOrderToDelivered = asyncHandler (async (req, res)=> {
-    res.send('update order to delivered');
-});
+// @desc    Update order to delivered
+// @route   GET /api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+  
+    if (order) {
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+  
+      const updatedOrder = await order.save();
+  
+      res.json(updatedOrder);
+    } else {
+      res.status(404);
+      throw new Error('Order not found');
+    }
+  });
 
 // @desc     Get all orders
 // @route    GET /api/orders
